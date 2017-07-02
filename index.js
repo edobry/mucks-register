e61bfb947c56ae1855508b6ef80f2c7a47212194
Eugene Dobry <edory@gmail.com>
2017-07-02 10:47:05 -0400

    fixed body

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ var LOG = message => console.log(message);
 module.exports = (route, appName, port) => {
     LOG(`registering route ${route} to ${appName}...`);
 
+    const body = { id: appName };
+
+    if(port)
+        body.port = port;
+
     return request.post({
         url: `http://localhost:1999/${route}`,
-        body: { id: appName, port }
-    }).catch(err =>
-        console.log(`${err}: mucks not found?`))
-    .tap(port =>
+        json: true,
+        body
+    }).catch(err => {
+        throw new Error(`${err}: mucks not found?`)
+    }).tap(port =>
         LOG(`registered ${appName} to localhost:${port}/${route}`));
 };
